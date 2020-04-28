cf85f87a8e775be56c851ffc1985178f82fcfb20
Viviane Miranda <vivianehfm@gmail.com>
2020-04-28 16:57:29 -0300

    inclusão das consts dentro do escopo da função additens, tentar incluir event target pra resolver o escopo das outras funções

--- script.js
+++ script.js
@@ -20,52 +20,55 @@ window.addEventListener('DOMContentLoaded', function () {
     // console.log (lista)
 
 
-    //criando botão para excluir item
-    const botaoExcluir = document.createElement('button')
-    botaoExcluir.innerText = 'X'
-
+    
     //criando a div do item da lista
     // const divTarefas = document.querySelector('#todoTarefas')
-
-    const item = document.createElement('li')
-
-    function addItens(e) {
+    
+    
+    function addItens() {
         // 4- validar input não vazio
-        if (input.value.trim()!== '') {
-            e.preventDefault()
+        if (input.value.trim() !== '') {
+            const item = document.createElement('li')
             item.innerText = input.value
             lista.appendChild(item)
-            // console.log (item)
-                        
+            console.log (item)
+            
+            //criando botão para excluir item
+            const botaoExcluir = document.createElement('button')
+            botaoExcluir.innerText = 'X'
             item.appendChild(botaoExcluir)
             // console.log (botaoExcluir)
-
+            
             // 5- resetar input
-            // input.reset()
+            input.reset()
         } else {
-            alert ('Escreva uma tarefa!')
+            alert('Escreva uma tarefa!')
         }
     }
-
+    
     
     //resolução com for
     // function addItens(e) {
-    //     for (i = lista.length - 1; i >= 0; i--) {
-    //         if (input.value.length !== 0 || input.value.trim()) {
-    //             e.preventDefault()
-    //             item.innerText = input.value
-    //             item = (lista[i])
-    //             lista.appendChild(item)
-    //             // console.log (item)
-
-    //             item.appendChild(botaoExcluir)
-    //             // console.log (botaoExcluir)
-    //         }
-    //     }
-    // }
-
-    addTarefas.addEventListener('click', addItens)
-
+        //     for (i = lista.length - 1; i >= 0; i--) {
+            //         if (input.value.trim() !== '') {
+                //             e.preventDefault()
+                //             item.innerText = input.value
+                //             item = (lista[i])
+                //             lista.appendChild(item)
+                //             // console.log (item)
+                
+                //             item.appendChild(botaoExcluir)
+                //             // console.log (botaoExcluir)
+                //         }
+                //     }
+                // }
+                
+                addTarefas.addEventListener('click', function(e){
+                    e.preventDefault() 
+                    addItens()
+                })
+               
+                
     // Marcar itens concluídos individualmente
     function itemConcluido() {
         item.classList.add('itemConcluido');
@@ -76,17 +79,38 @@ window.addEventListener('DOMContentLoaded', function () {
 
     //3 - Excluir itens individualmente
 
-    function excluirItem (){
-        lista.removeChild (item)
+    function excluirItem() {
+        lista.removeChild(item)
     }
 
-    botaoExcluir.addEventListener ('click', excluirItem)
+    botaoExcluir.addEventListener('click', excluirItem)
 
 
 
 
 
+    // **BÔNUS**
 
+    // - Marcar todos os itens como feito
+    // - Excluir todos os itens da lista   
+
+
+    const botaoMarcarTodos = document.querySelector ('#todoMarcarTodos')
+    const botaoLimparTodos = document.querySelector ('#todoRemoverTodos')
+
+    function limparTodos (){
+        item.innerText = ''
+    }
+
+    botaoLimparTodos.addEventListener ('click', limparTodos)
+
+
+    function marcarTodos (){
+      
+       lista.classList.add ('itemConcluido')
+    }
+
+    botaoMarcarTodos.addEventListener ('click', marcarTodos)
 
 })
 
