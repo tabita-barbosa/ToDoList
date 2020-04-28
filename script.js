73dd1d6cdf38a63f60e8bb2678b13c332b51c403
Viviane Miranda <vivianehfm@gmail.com>
2020-04-28 17:48:23 -0300

    bug do escopo de função resolvido

--- script.js
+++ script.js
@@ -20,97 +20,81 @@ window.addEventListener('DOMContentLoaded', function () {
     // console.log (lista)
 
 
-    
+
     //criando a div do item da lista
     // const divTarefas = document.querySelector('#todoTarefas')
-    
-    
+
+
     function addItens() {
         // 4- validar input não vazio
+        const item = document.createElement('li')
+        const botaoExcluir = document.createElement('button')
         if (input.value.trim() !== '') {
-            const item = document.createElement('li')
             item.innerText = input.value
             lista.appendChild(item)
-            console.log (item)
-            
+            console.log(item)
+
             //criando botão para excluir item
-            const botaoExcluir = document.createElement('button')
             botaoExcluir.innerText = 'X'
             item.appendChild(botaoExcluir)
             // console.log (botaoExcluir)
-            
+
             // 5- resetar input
-            input.reset()
+            // input.reset()
         } else {
             alert('Escreva uma tarefa!')
         }
-    }
-    
-    
-    //resolução com for
-    // function addItens(e) {
-        //     for (i = lista.length - 1; i >= 0; i--) {
-            //         if (input.value.trim() !== '') {
-                //             e.preventDefault()
-                //             item.innerText = input.value
-                //             item = (lista[i])
-                //             lista.appendChild(item)
-                //             // console.log (item)
-                
-                //             item.appendChild(botaoExcluir)
-                //             // console.log (botaoExcluir)
-                //         }
-                //     }
-                // }
-                
-                addTarefas.addEventListener('click', function(e){
-                    e.preventDefault() 
-                    addItens()
-                })
-               
-                
-    // Marcar itens concluídos individualmente
-    function itemConcluido() {
-        item.classList.add('itemConcluido');
+        // Marcar itens concluídos individualmente
+        function itemConcluido() {
+            item.classList.add('itemConcluido');
+        }
+
+        item.addEventListener('click', itemConcluido)
+
+
+        //3 - Excluir itens individualmente
+
+        function excluirItem() {
+            lista.removeChild(item)
+
+        }
+
+        botaoExcluir.addEventListener('click', excluirItem)
+
+        // **BÔNUS**
+
+        // - Marcar todos os itens como feito
+        // - Excluir todos os itens da lista   
+
+
+        const botaoMarcarTodos = document.querySelector('#todoMarcarTodos')
+        const botaoLimparTodos = document.querySelector('#todoRemoverTodos')
+
+        function limparTodos() {
+            item.innerText = ''
+        }
+
+        botaoLimparTodos.addEventListener('click', limparTodos)
+
+
+        function marcarTodos() {
+
+            lista.classList.add('itemConcluido')
+        }
+
+        botaoMarcarTodos.addEventListener('click', marcarTodos)
 
     }
 
-    item.addEventListener('click', itemConcluido)
 
-    //3 - Excluir itens individualmente
 
-    function excluirItem() {
-        lista.removeChild(item)
-    }
-
-    botaoExcluir.addEventListener('click', excluirItem)
+    addTarefas.addEventListener('click', function (e) {
+        e.preventDefault()
+        addItens()
+    })
 
 
 
-
-
-    // **BÔNUS**
-
-    // - Marcar todos os itens como feito
-    // - Excluir todos os itens da lista   
-
-
-    const botaoMarcarTodos = document.querySelector ('#todoMarcarTodos')
-    const botaoLimparTodos = document.querySelector ('#todoRemoverTodos')
-
-    function limparTodos (){
-        item.innerText = ''
-    }
-
-    botaoLimparTodos.addEventListener ('click', limparTodos)
-
-
-    function marcarTodos (){
-      
-       lista.classList.add ('itemConcluido')
-    }
-
-    botaoMarcarTodos.addEventListener ('click', marcarTodos)
 
 })
 
@@ -119,3 +103,18 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
 
+//resolução com for
+    // function addItens(e) {
+        //     for (i = lista.length - 1; i >= 0; i--) {
+            //         if (input.value.trim() !== '') {
+                //             e.preventDefault()
+                //             item.innerText = input.value
+                //             item = (lista[i])
+                //             lista.appendChild(item)
+                //             // console.log (item)
+
+                //             item.appendChild(botaoExcluir)
+                //             // console.log (botaoExcluir)
+                //         }
+                //     }
+                // }
